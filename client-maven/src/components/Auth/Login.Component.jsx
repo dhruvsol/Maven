d1ec7c198db29588fc2caa9a63843d48042a0b92
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-27 21:27:47 +0530

    make the login page

--- client-maven/src/components/Auth/Login.Component.jsx
+++ client-maven/src/components/Auth/Login.Component.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import './Login.Style.scss'
+import styled from 'styled-components'
 export const Login = () => {
     return (
-        <div>
-            This is login page
-        </div>
+        <Container className='Login'>
+            <div className='Login-card'>
+                <div className='Login-heading'>
+                    <span className='Login-welcome'>Welcome to<strong> MAVEN</strong></span>
+                    <span><p>No Account ?</p><a href="/signup">Signup</a></span>
+                </div>
+            </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+background: url("/images/bg.png");
+`
