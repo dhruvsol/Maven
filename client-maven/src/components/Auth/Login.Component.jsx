527e285a9fe591392ec7b225538f3ac9f7b34ce8
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-28 09:37:29 +0530

    complete the login pages

--- client-maven/src/components/Auth/Login.Component.jsx
+++ client-maven/src/components/Auth/Login.Component.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
-import './Login.Style.scss'
-import styled from 'styled-components'
+import React from "react";
+import "./Login.Style.scss";
+import styled from "styled-components";
 export const Login = () => {
-    return (
-        <Container className='Login'>
-            <div className='Login-card'>
-                <div className='Login-heading'>
-                    <span className='Login-welcome'>Welcome to<strong> MAVEN</strong></span>
-                    <span><p>No Account ?</p><a href="/signup">Signup</a></span>
-                </div>
-            </div>
-        </Container>
-    )
-}
+  return (
+    <Container className="Login">
+      <div className="Login-card">
+        <div className="Login-heading">
+          <span className="Login-welcome">
+            Welcome to<strong> MAVEN</strong>
+          </span>
+          <span>
+            <p>No Account ?</p>
+            <a href="/signup">Signup</a>
+          </span>
+        </div>
+        <div className="Login-input">
+          <h1>Sign in</h1>
+          <div className="data-input">
+              <label > Email</label>
+            <input type="email"  placeholder="example@mail.com" />
+            <br />
+            <label > Password</label>
+            <input type="password"  placeholder="Password" />
+          </div>
+          <div className="Login-summit">
+            <a href="#">Forgot Password </a>
+
+            <button type="summit"> Sign in</button>
+          </div>
+          <div className="Login-google">
+              <span>Or continue with</span>
+              <img src="/images/google.png" alt="google"/>
+
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-background: url("/images/bg.png");
-`
+  background: url("/images/bg.png");
+`;
